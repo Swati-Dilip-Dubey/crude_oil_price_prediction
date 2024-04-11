e72c01f758e1f30dfd361b35aca101a07e1d1f6c
Swati-Dilip-Dubey <pinkychand87@gmail.com>
2024-04-11 14:21:16 +0530

    model trainer added

---
+++ src/mlProject/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.data_transformation import DataTransformation
+from mlProject import logger
+
+STAGE_NAME = "Data Transformation stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        try:
+            # Read validation status 
+            with open(Path("artifacts/data_validation/status.txt"), "r") as f:
+                status = f.read().split(" ")[-1]
+            
+            if status == "True":
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_spliting()
+            
+            else:
+                raise Exception("Your data schema is not valid.")
+
+        except Exception as e:
+            raise e
+        
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+        obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nX==================X")
+    except Exception as e:
+        logger.exception(e)
+        raise e
